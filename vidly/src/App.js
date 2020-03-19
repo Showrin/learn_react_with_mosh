573926c3cf215eb4420f8c62cd5917d625a38f73
Showrin Barua <showrin.barua@welldev.io>
2020-03-19 18:44:18 +0600

    Section-5 Lesson-2

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Movies from "./components/movies";
+import Pagination from "./components/pagination";
 
 function App() {
 	return (
 		<main className='container'>
 			<Movies />
+			<Pagination />
 		</main>
 	);
 }
