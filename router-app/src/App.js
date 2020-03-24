4609acc5c964990089bed21e560a264fe3e40eec
Showrin <showrinece15@gmail.com>
2020-03-24 07:53:46 +0600

    Section 6 Lesson 3-5

--- router-app/src/App.js
+++ router-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Posts from "./components/posts";
@@ -9,13 +10,21 @@ import NotFound from "./components/notFound";
 import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <NavBar />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<NavBar />
+				<div className='content'>
+					<Switch>
+						<Route path='/products' component={Products} />
+						<Route path='/posts' component={Posts} />
+						<Route path='/admin' component={Dashboard} />
+						<Route path='/' component={Home} />
+					</Switch>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
