7029fdad0ca7306ba6b43a49f054f6f20798d02a
Showrin (Personal) <showrinece15@gmail.com>
2020-03-26 00:24:40 +0600

    Section 6 Lesson 8-13

--- router-app/src/App.js
+++ router-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Posts from "./components/posts";
@@ -26,9 +26,12 @@ class App extends Component {
 								<Products newProp="Hello" {...props} />
 							)}
 						/>
-						<Route path="/posts/:year/:month" component={Posts} />
+						<Route path="/posts/:year?/:month?" component={Posts} />
+						<Redirect from="/messages" to="/posts" />
 						<Route path="/admin" component={Dashboard} />
-						<Route path="/" component={Home} />
+						<Route path="/not-found" component={NotFound} />
+						<Route path="/" exact component={Home} />
+						<Redirect to="/not-found" />
 					</Switch>
 				</div>
 			</div>
--- router-app/src/components/admin/dashboard.jsx
+++ router-app/src/components/admin/dashboard.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import Sidebar from "./sidebar";
+import Users from "./users";
+import Posts from "./posts";
 
 const Dashboard = ({ match }) => {
-  return (
-    <div>
-      <h1>Admin Dashboard</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Admin Dashboard</h1>
+			<Sidebar />
+			<Route path="/admin/posts" component={Posts} />
+			<Route path="/admin/users" component={Users} />
+		</div>
+	);
 };
 
 export default Dashboard;
